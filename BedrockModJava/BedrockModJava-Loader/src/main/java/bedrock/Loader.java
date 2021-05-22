562dcebe1c9ee4dd0980c090ebdc70cc23643e40
x7airworker <jake@jakes-co.de>
2021-05-22 14:23:23 +0200

    Added basic event handler

--- BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
+++ BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
@@ -4,4 +4,9 @@ public class Loader {
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
     }
+
+    public static void fireEvent(String name/*, Object... args*/) {
+        System.out.println("Received " + name);
+        // TODO: fire Event through Java EventBus
+    }
 }

bce725acc7f0d3980600f2dcf8b14f1db37d613a
x7airworker <jake@jakes-co.de>
2021-05-22 14:50:50 +0200

    Added args to fireEvent

--- BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
+++ BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
@@ -1,12 +1,14 @@
 package bedrock;
 
+import java.util.Arrays;
+
 public class Loader {
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
     }
 
-    public static void fireEvent(String name/*, Object... args*/) {
-        System.out.println("Received " + name);
+    public static void fireEvent(String name, Object... args) {
+        System.out.println("Received " + name + " " + Arrays.toString(args));
         // TODO: fire Event through Java EventBus
     }
 }

0a389d5f8531fd39cd93d4745004e1afad3af725
x7airworker <jake@jakes-co.de>
2021-05-22 13:30:19 +0200

    Removed FunctionPointer example

--- BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
+++ BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
@@ -3,7 +3,5 @@ package bedrock;
 public class Loader {
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
-        FunctionPointer<Void> ptr = new FunctionPointer<>(0x0075ef50);
-        ptr.invoke(Void.class, "Hallo über printf");
     }
 }

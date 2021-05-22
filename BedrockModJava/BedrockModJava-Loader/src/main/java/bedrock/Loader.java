94e07f072398e4640f2304ad5e3e31196b3678c3
x7airworker <jake@jakes-co.de>
2021-05-22 15:03:22 +0200

    Changed signature and implementation of fireEvent

--- BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
+++ BedrockModJava/BedrockModJava-Loader/src/main/java/bedrock/Loader.java
@@ -1,14 +1,15 @@
 package bedrock;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Loader {
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
     }
 
-    public static void fireEvent(String name, Object... args) {
-        System.out.println("Received " + name + " " + Arrays.toString(args));
+    public static void fireEvent(String name, List<Object> args) {
+        System.out.println("Received " + name + " " + args.stream().map(Object::toString).collect(Collectors.joining(",")));
         // TODO: fire Event through Java EventBus
     }
 }

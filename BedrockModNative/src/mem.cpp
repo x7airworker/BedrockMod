07cc9d237ef6cc178b2f6f07b819c6a744759520
x7airworker <jake@jakes-co.de>
2021-10-03 16:47:13 +0200

    Added ClassBuilder as build step

---
+++ BedrockModNative/src/mem.cpp
@@ -0,0 +1,75 @@
+#include "mem.h"
+
+uintptr_t Mem::FindMLvlPtr(uintptr_t baseAddr, std::vector<unsigned int> offsets) {
+	uintptr_t addr = baseAddr;
+	for (int I = 0; I < offsets.size(); I++) {
+		addr = *(uintptr_t*)(addr);
+		if ((uintptr_t*)(addr) == nullptr)
+			return addr;
+		addr += offsets[I];
+	}
+	return addr;
+}
+
+auto Mem::getModuleBase() -> long long {
+	return (long long)GetModuleHandleA(nullptr);
+}
+
+auto Mem::getModuleBaseHandle() -> HMODULE {
+	return GetModuleHandleA(nullptr);
+}
+
+auto Mem::getBaseModuleSize() -> long long {
+	return (long long) GetModuleHandle(0);
+}
+
+auto Mem::getBaseModuleEnd() -> long long {
+	return getModuleBase() + getBaseModuleSize();
+}
+
+void Mem::SetThisModule(HMODULE mod) {
+	thisMod = mod;
+}
+auto Mem::GetThisModule() -> HMODULE {
+	return thisMod;
+}
+
+auto Mem::FindSig(const char* pattern) -> uintptr_t {
+	return FindSig(getModuleBase(), getBaseModuleEnd(), pattern);
+}
+struct SearchedSig {
+	std::string pattern;
+	uintptr_t result;
+	SearchedSig(std::string pattern, uintptr_t result) {
+		this->pattern = pattern;
+		this->result = result;
+	}
+};
+
+std::vector<SearchedSig> alreadySearched = std::vector<SearchedSig>();
+auto Mem::FindSig(long long rangeStart, long long rangeEnd, const char* pattern) -> uintptr_t {
+	for (int i = 0; i < alreadySearched.size(); i++) {
+		if (alreadySearched[i].pattern == std::string(pattern)) {
+			return alreadySearched[i].result;
+		}
+	}
+	const char* pat = pattern;
+	long long firstMatch = 0;
+	for (long long pCur = rangeStart; pCur < rangeEnd; pCur++) {
+		if (!*pat) return firstMatch;
+		if (*(PBYTE)pat == '\?' || *(BYTE*)pCur == getByte(pat)) {
+			if (!firstMatch) firstMatch = pCur;
+			if (!pat[2]) {
+				alreadySearched.push_back(SearchedSig(std::string(pattern), firstMatch));
+				return firstMatch;
+			};
+			if (*(PWORD)pat == '\?\?' || *(PBYTE)pat != '\?') pat += 3;
+			else pat += 2;
+		}
+		else {
+			pat = pattern;
+			firstMatch = 0;
+		}
+	}
+	return 0;
+}

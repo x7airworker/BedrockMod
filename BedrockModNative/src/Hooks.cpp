f57bdb56ec8dcb1d0d65c983bf93af95fd22a23d
x7airworker <jake@jakes-co.de>
2021-10-12 00:21:39 +0200

    Now using Detours on Windows and Subhook on Linux

--- BedrockModNative/src/Hooks.cpp
+++ BedrockModNative/src/Hooks.cpp
@@ -1,32 +1,59 @@
 #include "Hooks.h"
-#include <windows.h>
-#include <detours.h>
 #include <iostream>
 #include "BedrockMod.h"
 #include "bedrock.h"
 
-void hkMinecraftInit(Minecraft* instance)
+#ifdef __linux__
+#define IMPL_TRAMPOLINE(NAME) subhook::ScopedHookRemove remove(&NAME)
+#else
+#define IMPL_TRAMPOLINE(NAME)
+#endif
+
+Hooks::Hook minecraftInitHook;
+Hooks::Hook minecraftUpdateHook;
+Hooks::Hook networkHandlerSendHook;
+
+void __thiscall hkMinecraftInit(Minecraft* instance)
 {
+	std::cout << "Before remove" << std::endl;
+	IMPL_TRAMPOLINE(minecraftInitHook);
+
 	instance->init();
 }
 
-void hkMinecraftUpdate(Minecraft* instance)
+void __thiscall hkMinecraftUpdate(Minecraft* instance)
 {
+	IMPL_TRAMPOLINE(minecraftUpdateHook);
+
 	instance->update();
 }
 
-void hkNetworkHandlerSend(NetworkHandler* handler, NetworkIdentifier* i, Packet* packet, unsigned char flags)
+void __thiscall hkNetworkHandlerSend (NetworkHandler* handler, NetworkIdentifier* i, Packet* packet, unsigned char flags)
 {
+	IMPL_TRAMPOLINE(networkHandlerSendHook);
 	std::cout << "Sended packet: " << packet->getId() << std::endl;
 	handler->send(handler, i, packet, flags);
 }
 
 void Hooks::Install()
 {
+	minecraftInitHook = Hook((void*) &Minecraft::holder_init,(void*) &hkMinecraftInit);
+	minecraftInitHook.install();
+	minecraftUpdateHook = Hook((void*)&Minecraft::holder_update, (void*)&hkMinecraftUpdate);
+	minecraftUpdateHook.install();
+	networkHandlerSendHook = Hook((void*)&NetworkHandler::holder_send, (void*)&hkNetworkHandlerSend);
+	networkHandlerSendHook.install();
+}
+
+void Hooks::Hook::install()
+{
+#ifdef _WIN32
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
-	DetourAttach(&(LPVOID&)Minecraft::holder_init, &hkMinecraftInit);
-	DetourAttach(&(LPVOID&)Minecraft::holder_update, &hkMinecraftUpdate);
-	DetourAttach(&(LPVOID&)NetworkHandler::holder_send, &hkNetworkHandlerSend);
+	DetourAttach((PVOID*)func, (PVOID)hookFunc);
 	DetourTransactionCommit();
+#elif __linux__
+	hook = subhook::Hook();
+	hook.Install((void*)func, (void*)hookFunc);
+#endif
 }
--- BedrockModNative/src/Hooks.h
+++ BedrockModNative/src/Hooks.h
@@ -1,6 +1,29 @@
 #pragma once
 
+#ifdef __linux__
+#include <subhook.h>
+#elif _WIN32
+#include <Windows.h>
+#include <Detours.h>
+#endif
+
 namespace Hooks
 {
 	void Install();
+
+	class Hook
+	{
+	private:
+		void* func;
+		void* hookFunc;
+#ifdef __linux__
+		subhook::Hook hook;
+#endif
+
+	public:
+		Hook() {}
+		Hook(void* func, void* hookFunc) : func(func), hookFunc(hookFunc) {}
+
+		void install();
+	};
 };
